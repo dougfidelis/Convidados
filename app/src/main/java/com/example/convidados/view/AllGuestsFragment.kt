f2ae76ebd2fa2c87b106d28ac6d0d0a882ab27eb
Douglas <doug.fidelis@gmail.com>
2023-01-21 14:24:40 -0300

    feat: add remove guest function

--- app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
+++ app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.convidados.databinding.FragmentAllGuestsBinding
 import com.example.convidados.view.adapter.GuestsAdapter
+import com.example.convidados.view.listener.OnGuestListener
 import com.example.convidados.viewmodel.AllGuestsViewModel
 
 
@@ -29,6 +28,21 @@ class AllGuestsFragment : Fragment() {
         binding.recyclerAllGuests.layoutManager = LinearLayoutManager(context)
 
         binding.recyclerAllGuests.adapter = guestsAdapter
+
+        val listener = object : OnGuestListener {
+            override fun onClick(id: Int) {
+                Toast.makeText(context, "Clicou $id", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onDelete(id: Int) {
+                viewModel.delete(id)
+                viewModel.getAll()
+
+            }
+
+        }
+
+        guestsAdapter.attachListener(listener)
 
         viewModel.getAll()
 
