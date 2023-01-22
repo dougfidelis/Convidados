feae0c524f3ecb49492be0daacf3b19316267266
Douglas <doug.fidelis@gmail.com>
2023-01-22 20:31:01 -0300

    feat: add update guest

--- app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
+++ app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
@@ -1,14 +1,15 @@
 package com.example.convidados.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.convidados.databinding.FragmentAllGuestsBinding
+import com.example.convidados.repository.DataBaseConstants
 import com.example.convidados.view.adapter.GuestsAdapter
 import com.example.convidados.view.listener.OnGuestListener
 import com.example.convidados.viewmodel.AllGuestsViewModel
@@ -22,7 +23,7 @@ class AllGuestsFragment : Fragment() {
     private val guestsAdapter = GuestsAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, b: Bundle?): View {
-        viewModel = ViewModelProvider(this).get(AllGuestsViewModel::class.java)
+        viewModel = ViewModelProvider(this)[AllGuestsViewModel::class.java]
         _binding = FragmentAllGuestsBinding.inflate(inflater, container, false)
 
         binding.recyclerAllGuests.layoutManager = LinearLayoutManager(context)
@@ -31,23 +32,30 @@ class AllGuestsFragment : Fragment() {
 
         val listener = object : OnGuestListener {
             override fun onClick(id: Int) {
-                Toast.makeText(context, "Clicou $id", Toast.LENGTH_SHORT).show()
+                val intent = Intent(context, GuestsFormActivity::class.java)
+                val bundle = Bundle()
+
+                bundle.putInt(DataBaseConstants.GUEST.ID, id)
+                intent.putExtras(bundle)
+
+                startActivity(intent)
             }
 
             override fun onDelete(id: Int) {
                 viewModel.delete(id)
                 viewModel.getAll()
             }
-
         }
 
         guestsAdapter.attachListener(listener)
-
-        viewModel.getAll()
-
         observe()
 
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getAll()
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/convidados/view/GuestsFormActivity.kt
+++ app/src/main/java/com/example/convidados/view/GuestsFormActivity.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.convidados.R
 import com.example.convidados.databinding.ActivityGuestFormBinding
 import com.example.convidados.model.GuestModel
+import com.example.convidados.repository.DataBaseConstants
 import com.example.convidados.repository.GuestsRepository
 import com.example.convidados.viewmodel.GuestsFormViewModel
 
@@ -15,25 +16,50 @@ class GuestsFormActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityGuestFormBinding
     private lateinit var viewModel: GuestsFormViewModel
 
+    private var guestId = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityGuestFormBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel = ViewModelProvider(this).get(GuestsFormViewModel::class.java)
+        viewModel = ViewModelProvider(this)[GuestsFormViewModel::class.java]
 
         binding.confirmationButton.setOnClickListener(this)
         binding.radioPresent.isChecked = true
+
+        observe()
+        loadData()
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.confirmation_button) {
             val name = binding.editName.text.toString()
             val presence = binding.radioPresent.isChecked
-            val guest = GuestModel(0, name, presence)
-            viewModel.insert(guest)
 
+            val guest = GuestModel(guestId, name, presence)
+            viewModel.save(guest)
+            finish()
+        }
+    }
+
+    private fun observe() {
+        viewModel.guest.observe(this) {
+            binding.editName.setText(it.name)
+            if (it.presence) {
+                binding.radioPresent.isChecked = true
+            } else {
+                binding.radioAbsent.isChecked = true
+            }
+        }
+    }
+
+    private fun loadData() {
+        val bundle = intent.extras
+        if (bundle != null) {
+            guestId = bundle.getInt(DataBaseConstants.GUEST.ID)
+            viewModel.get(guestId)
         }
     }
 }
